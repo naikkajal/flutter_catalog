7c7b82393ef6722e86d05c6f42959aded649ce04
Kajal Naik <naikkajal0603@gmail.com>
2024-02-25 18:18:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+            child: Container(
+          child: Text("Welcome"),
+        )),
+      ),
+    );
+  }
+}
