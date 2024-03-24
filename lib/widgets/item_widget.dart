f92dd32bd22555f63cba5da80e82f96665b0d82c
Kajal Naik <naikkajal0603@gmail.com>
2024-03-24 22:17:21 +0530

    json

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -4,9 +4,8 @@ import 'package:flutter_catalog/models/catalog.dart';
 class ItemWidget extends StatelessWidget {
   final Item item;
 
-  const ItemWidget({required Key key, required this.item})
-      : assert(item != null),
-        super(key: key);
+  const ItemWidget({Key? key, required this.item})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
